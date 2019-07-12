7a4f3b76411082586d87bd2d3d3deb15d75d2ba1
Sam <samdeniyi@gmail.com>
2019-07-12 11:17:43 +0100

    modified the env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,8 @@ export const environment = {
   supportedLanguages: ['en-US', 'fr-FR'],
   showLogo: true,
   payStackBaseUrl: 'https://api.paystack.co/',
-  publicKey: 'pk_test_e338dfb13a1bc3e711759520fb73202a5491b0ba',
+  // publicKey: 'pk_test_e338dfb13a1bc3e711759520fb73202a5491b0ba',
+  publicKey: 'pk_live_2342b6e05e7a95f7edf9d0a37ccf6238c9366645',
   initailDebt: '5000',
   featureLoan: 2
 };
