c15574bc4b2c3ea3d336389740619d4b3b01b237
Sam <samdeniyi@gmail.com>
2019-09-06 19:32:05 +0100

    gf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,6 +20,7 @@ export const environment = {
   showLogo: false,
   payStackBaseUrl: 'https://api.paystack.co/',
   publicKey: 'pk_live_2342b6e05e7a95f7edf9d0a37ccf6238c9366645',
+
   // publicKey: 'pk_test_e338dfb13a1bc3e711759520fb73202a5491b0ba',
   initailDebt: '50',
   featureLoan: 4
